dd0696ef71515a9881c6d97440356606cfbaace8
Adornos <guilhermeaapassos@gmail.com>
2026-03-18 12:02:28 -0300

    Enhance game and console pages with detailed information and improved layout

--- aula-02/app.py
+++ aula-02/app.py
@@ -15,11 +15,21 @@ def home():
 
 @app.route('/games')
 def games():
-    return render_template('games.html')
+    #Variaveis da pagina de gaems
+    
+    titulo = 'SilkSong'
+    descricao ='SilkSong é um jogo de ação e aventura desenvolvido pela Team Cherry, conhecido por seu estilo artístico encantador e jogabilidade desafiadora. O jogo se passa em um mundo subterrâneo repleto de criaturas misteriosas e ambientes deslumbrantes. Os jogadores assumem o papel de uma pequena criatura chamada Hornet, que embarca em uma jornada para descobrir os segredos do reino de Hallownest. Com uma combinação de exploração, combate e habilidades únicas, SilkSong promete oferecer uma experiência envolvente para os fãs de jogos indie e amantes de aventuras emocionantes.'
+    ano = 2025
+    categoria = 'Metroidvania'
+    jogadores = ['Guilherme', 'Maria', 'João', 'Ana']
+
+    return render_template('games.html', titulo=titulo, descricao=descricao, ano=ano, categoria=categoria, jogadores=jogadores)
 
 @app.route('/consoles')
-def consoles():
-    return render_template('consoles.html')
+def consoles(): 
+    consolesDestaque= ['Nintendo Switch 2']
+    consoles= ['Playstation 1', 'Playstation 2', 'Playstation 4', 'Playstation 5', 'Playstation 5']
+    return render_template('consoles.html', consoles=consoles, consolesDestaque=consolesDestaque)
 # def serve para criar funções no pythone home é o nome da função
 
 
